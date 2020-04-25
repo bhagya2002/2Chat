620fe42ac3f1e1bbacf37808fb2f485345169472
bhagya2002 <60622922+bhagya2002@users.noreply.github.com>
2020-04-25 15:36:45 -0600

    user make an account php changes

--- signup_user.php
+++ signup_user.php
@@ -1,7 +1,13 @@
 <?php
-include("include/connection.php")
+// Starts session
+session_start();
 
+// connects to the database
+include("include/connection.php");
+
+// get information and run when the submit button is clicked
 if(isset($_POST['sign_up'])) {
+    // get values and make sure it is safe
     $name = htmlentities(mysqli_real_escape_string($con, $_POST['user_name']));
     $pass = htmlentities(mysqli_real_escape_string($con, $_POST['user_pass']));
     $email = htmlentities(mysqli_real_escape_string($con, $_POST['user_email']));
@@ -9,38 +15,45 @@ if(isset($_POST['sign_up'])) {
     $gender = htmlentities(mysqli_real_escape_string($con, $_POST['user_gender']));
     $rand = rand(1,2);
 
+    // run form validation
     if($name =='') {
         echo"<scipt>alert('We can not verify your name')</script>";
     }
     if(strlen($pass) < 8) {
-        echo"<scipt>alert('Password should be a minimum 8 characters!')</script>";
+        echo"<script>alert('Password should be a minimum 8 characters!')</script>";
         exit();
     }
 
+      // first sql query to check if a user exists when a specific email and password combination is entered
     $check_email = "SELECT * from users where user_email = '$email'";
+    // this is used to run the search 
     $run_email = mysqli_query($con, $check_email);
-
+// data returned with a user with the same information
     $check = mysqli_num_rows($run_email);
 
+    // if a user exists execute this code
     if($check == 1) {
         echo"<scipt>alert('Email already exists, please try again!')</script>";
-        echo"<scipt>window.open('signup.php', '_self')</script>";
+        echo"<scipt>window.open('index.php', '_self')</script>";
         exit();
     }
 
+    // choose a random profile picture for the user
     if ($rand == 1)
-        // $profile_pic = "images/avatar1.";
+        $profile_pic = "images/avatar1.png";
      else if ($rand == 2) 
-        // $profile_pic = "images/avatar2.";
+        $profile_pic = "images/avatar2.png";
 
+        // insert into the database
         $sql = "INSERT into users(user_name, user_pass, user_email, user_profile, user_country, user_gender) VALUES('$name', '$pass', '$email', '$profile_pic', '$country', '$gender')";
-
+// query that data
         $query = mysqli_query($con, $sql);
 
+        // once it runs successfully then execute this
         if ($query) {
             echo "<script>alert('Congratulations $name, your account has been created successfully')</script>";
             echo "<script>window.open('signin.php', '_self')</script>";
-        }
+        } // if not run this
         else {
             echo "<script>alert('Registration failed, try again!')</script>";
             echo "<script>window.open('signup.php', '_self')</script>";
