deff8b87622806c689a150c4ee7589f64425aa70
Bhagya Patel <60622922+bhagya2002@users.noreply.github.com>
2020-04-26 17:06:58 -0600

    updated file

--- find_friends_function.php
+++ find_friends_function.php
@@ -1,31 +1,28 @@
 <?php
-// connect to database
-$con = mysqli_connect('localhost', 'bhagya', 'test1234', 'mychat');
+$con = mysqli_connect('localhost', 'bhagya', 'test1234', 'mychat') or die("Connection was not established");
 
-// check connection 
-if (!$con) {
-    // if the conncetion fails to conenct to the server
-    echo 'Connection error: ' . mysqli_connect_error();
-}
-function search_user() {
-    // variables
-    global $con;
-
-    if(isset($_GET['search_btn'])) {
-        $search_qurey = htmlentities($_GET['search_query']);
-        $get_user = "SELECT * from users WHERE user_name LIKE '%$search_qurey%' or user_country LIKE '%$search_qurey%'";
-    }else {
-        $get_user = "SELECT * from users order by user_country, user_name DESC LIMIT 5";
-    }
-    $run_user = mysqli_qurey($con, $get_user);
-
-    while($row = mysqli_fetch_array($un_user)) {
-        $user_name = $row['user_name'];
-        $user_profile = $row['user_profile'];
-        $user_country = $row['user_country'];
-        $user_gender = $row['user_gender'];
-
-        //now displaying all at once 
+	function search_user(){
+	
+		global $con;
+		
+		if(isset($_GET['search_btn'])){
+		$search_query = htmlentities($_GET['search_query']);
+		$get_user = "select * from users where user_name like '%$search_query%' or user_country like '%$search_query%'";
+		}
+		else{
+		$get_user = "SELECT * from users ORDER BY user_country,user_name DESC LIMIT 5"; 
+		}
+		
+		$run_user = mysqli_query($con,$get_user);
+		
+		while($row_user=mysqli_fetch_array($run_user)){
+			
+		  $user_name = $row_user['user_name'];
+		  $user_profile = $row_user['user_profile'];
+		  $country = $row_user['user_country'];
+		  $gender = $row_user['user_gender'];
+			
+			//now displaying all at once 
 			
 			echo "
 			<div class='card'>
