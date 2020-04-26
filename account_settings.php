014de8fae968c638b7d146e21963416b779fcfb2
bhagya2002 <60622922+bhagya2002@users.noreply.github.com>
2020-04-26 17:42:22 -0600

    account settings

---
+++ account_settings.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<?php
+session_start();
+include("include/connection.php");
+include("header.php");
+
+if(!isset($_SESSION['user_email'])){
+	
+	header("location: index.php");
+
+}
+else{ ?>
+<html>
+
+<head>
+    <title>Account Settings</title>
+    <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</head>
+<body>
+    
+</body>
+</html>
