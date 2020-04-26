b9db865d0eb0167826d48f999864296a082d7d94
bhagya2002 <60622922+bhagya2002@users.noreply.github.com>
2020-04-26 16:49:32 -0600

    title changes for devices

--- home.php
+++ home.php
@@ -11,6 +11,9 @@ if(!isset($_SESSION['user_email'])){
 else{ ?>
 <html>
 <head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MyChat</title>
 	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="css/home.css">
 	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
