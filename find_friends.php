5ea1bf18640788d3bba39b31b5d33bfb983808dc
bhagya2002 <60622922+bhagya2002@users.noreply.github.com>
2020-04-26 18:09:44 -0600

    set a header

--- find_friends.php
+++ find_friends.php
@@ -1,5 +1,27 @@
 <!DOCTYPE html>
+<?php
+session_start();
+include("find_friends_function.php");
+include("include/connection.php");
 
+if(!isset($_SESSION['user_email'])){
+	
+	header("location: index.php");
+
+}
+else{ ?>
+<html>
+
+<head>
+    <title>MyChat Friends</title>
+    <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/find_people.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</head>
 
 <body>
     <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
