cb63a91efc4747861a8d77c17941c23aaad22af3
Bhagya Patel <60622922+bhagya2002@users.noreply.github.com>
2020-05-17 00:45:05 -0600

    bug fixed and comments added

--- include/get_users_data.php
+++ include/get_users_data.php
@@ -2,16 +2,22 @@
 // connect to database
 $con = mysqli_connect('localhost', 'bhagya', 'test1234', 'mychat');
 
-	$user = "select * from users";
+// query user
+	$user = "SELECT * from users";
 			
+	// get info
 	$run_user = mysqli_query($con,$user);
 			
+	// while there is info do this..
 	while ($row_user=mysqli_fetch_array($run_user)){
-			
+			// get the users data from database 
 	$user_id = $row_user['user_id'];
 	$user_name = $row_user['user_name'];
 	$user_profile = $row_user['user_profile'];
 	$login = $row_user['log_in'];
+	// display the users information in a user card
+	// the profile picture of the user along with the name
+	// then check is the user is online or offline and display that data
 	echo"
 	<li>
 		<div class='chat-left-img'>
@@ -24,7 +30,6 @@ $con = mysqli_connect('localhost', 'bhagya', 'test1234', 'mychat');
 			}else{
 			echo "<span><i class='fa fa-circle-o' aria-hidden='true'></i> Offline</span>";
 			}
-
 			"
 		</div>
 	</li>
